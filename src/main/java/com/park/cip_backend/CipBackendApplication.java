3264e040bdc7815bb907938bc8e85020f0880ab4
park <frog7431@naver.com>
2024-03-08 00:23:19 +0900

    Initial commit

---
+++ src/main/java/com/park/cip_backend/CipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.park.cip_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/park/cip_backend/CipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.cip_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
